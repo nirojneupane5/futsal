5ed32863ecd86de4b54fff848889e812b08dc9df
nirojneupane5 <nirojneupane5@gmail.com>
2024-05-15 23:00:17 +0545

    Players added with json format using map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import sabin from "../../public/players/sabin.jpg";
 import Image from "next/image";
+import { player } from "./player";
 export default function Home() {
   return (
     <>
@@ -7,22 +8,27 @@ export default function Home() {
         <h1 className="text-center text-2xl font-bold pt-4 text-black">
           Player List
         </h1>
-        <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-5 py-2">
-          <div className="shadow-lg rounded-md text-center">
-            <div className="overflow-hidden">
-              <Image
-                src={sabin}
-                alt="sabin profile"
-                className="object-cover h-52 w-full hover:scale-150 duration-1000 hover:object-top"
-              />
-            </div>
-            <h1 className="text-xl font-bold text-black py-2">
-              Name: Sabin Neupane
-            </h1>
-            <p className="text-xl font-semibold text-black">
-              Position: Forward/Keeper
-            </p>
-          </div>
+        <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-5 mx-4 sm:mx-0 pt-2">
+          {player &&
+            player.map((info, index) => (
+              <div className="shadow-lg rounded-md text-center" key={index}>
+                <div className="overflow-hidden">
+                  <Image
+                    src={info.img}
+                    alt={info.name}
+                    className="object-cover md:h-52 h-64 object-top w-full hover:scale-150 duration-1000"
+                    height={200}
+                    width={300}
+                  />
+                </div>
+                <h1 className="text-xl font-bold text-black py-2">
+                  Name: {info.name}
+                </h1>
+                <p className="text-xl font-semibold text-black">
+                  Position: {info.position}
+                </p>
+              </div>
+            ))}
         </div>
       </div>
     </>
